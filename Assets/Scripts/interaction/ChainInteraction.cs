7e1f0d112081337e68c45949f6c2cf4d4924b8e8
Szorader <michalgorecki2004@o2.pl>
2026-05-02 12:24:43 +0200

    kill clients

--- Assets/Scripts/interaction/ChainInteraction.cs
+++ Assets/Scripts/interaction/ChainInteraction.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using FMODUnity;
+using System.Collections;
 
 public class ChainInteraction : MonoBehaviour, IInteractable
 {
@@ -11,6 +12,18 @@ public class ChainInteraction : MonoBehaviour, IInteractable
     
     [Header("AUDIO")]
     [SerializeField] private EventReference killSound;
+    
+    [Header("VFX")]
+    [SerializeField] private GameObject smokeEffect;
+    private float smokeDuration = 15f;
+    
+    public Animator animator;
+    
+    public GameObject modelFries1;
+    public GameObject modelFries2;
+    public GameObject modelFries3;
+
+    public Transform point;
 
     public enum QueueType
     {
@@ -42,8 +55,9 @@ public class ChainInteraction : MonoBehaviour, IInteractable
             return false;
         }
         
+        StartCoroutine(KillSequence(client));
         // Audio
-        RuntimeManager.PlayOneShot(killSound, transform.position);
+        /*RuntimeManager.PlayOneShot(killSound, transform.position);
 
         // "zabicie" klienta
         //queueManager.RemoveClient(client);
@@ -68,8 +82,48 @@ public class ChainInteraction : MonoBehaviour, IInteractable
             // tutaj możesz wywołać np.
             // GameManager.EndDay();
         }
-
+*/
         return true;
+    }
+    private IEnumerator KillSequence(ClientController client)
+    {
+        
+        animator.SetTrigger("close");
+
+        
+        yield return new WaitForSeconds(1f);
+
+        
+        RuntimeManager.PlayOneShot(killSound, transform.position);
+
+        
+        if (smokeEffect != null)
+        {
+            GameObject fx = Instantiate(smokeEffect, client.transform.position, Quaternion.identity);
+            Instantiate(modelFries1, client.transform.position, Quaternion.identity);
+            Instantiate(modelFries2, client.transform.position, Quaternion.identity);
+            Instantiate(modelFries3, client.transform.position, Quaternion.identity);
+            Destroy(fx, smokeDuration);
+        }
+        
+        
+        if (client.isBadClient)
+        {
+            Debug.Log("Udane! Zabiłeś złego klienta");
+            queueManager.dayManager.GoodClient();
+            queueManager.basket.UpdateMoneyKill(20f);
+        }
+        else
+        {
+            Debug.Log("Zabiłeś dobrego klienta — koniec dnia");
+            queueManager.dayManager.WrongClient();
+        }
+
+        
+        yield return new WaitForSeconds(10f);
+
+        queueManager.KillClient(client);
+        animator.SetTrigger("open");
     }
 
     public string GetPrompt()
